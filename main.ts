5a749e2ac3c89e539b177bacb933c7615b23418a
JohannesKing <105158760+JohannesKing@users.noreply.github.com>
2022-05-08 14:19:55 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,100 @@
+input.onButtonPressed(Button.A, function () {
+    alarmed = 1
+    music.playMelody("C C5 C C5 - - - - ", 500)
+})
+input.onGesture(Gesture.Shake, function () {
+    if (alarmed == 1) {
+        alarmed = 0
+        Alarm = 1
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    alarmed = 0
+    Alarm = 0
+    music.stopAllSounds()
+    basic.clearScreen()
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+})
+let alarmed = 0
+let Alarm = 0
+Alarm = 0
+basic.showIcon(IconNames.Diamond)
+basic.showIcon(IconNames.SmallDiamond)
+music.playMelody("C D E F G A B C5 ", 500)
+basic.pause(5000)
+alarmed = 1
+music.playMelody("C C5 C C5 - - - - ", 500)
+basic.forever(function () {
+    if (alarmed == 1) {
+        basic.showLeds(`
+            # # . . .
+            # . . . .
+            . . . . .
+            . . . . #
+            . . . # #
+            `)
+        basic.showLeds(`
+            # # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # # #
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # . .
+            `)
+        basic.showLeds(`
+            . . . # #
+            . . . . #
+            . . . . .
+            # . . . .
+            # # . . .
+            `)
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            # . . . #
+            # . . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . . . #
+            # . . . #
+            # . . . #
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . #
+            . . . . #
+            . . . . #
+            `)
+    }
+})
+basic.forever(function () {
+    if (Alarm == 1) {
+        music.playMelody("C5 - C5 - C5 - C5 - ", 500)
+        basic.showIcon(IconNames.Chessboard)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
